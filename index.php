541c035408bac8c8f849829129e5d92b23616d52
PabloPintor <pablo@pablopintor.com>
2020-07-13 16:55:49 +0200

    Actualizacion con dos juegos funcional

--- index.php
+++ index.php
@@ -2,36 +2,51 @@
 //mostrar formulario
 echo "<form action='index.php' method='post'>
 
-        <button type='submit'>Pregunta aleatoria</button>
+        <button name='boton' type='submit' value='pregunta'>Pregunta aleatoria</button>
+        <button name='boton' type='submit' value='probable'>¿Quien es mas probable que...?</button>
 
     </form>";
 
-//abrir y leer fichero
-$nombreFichero = "preguntas.txt";
-$fichero = fopen($nombreFichero, "r");
+//abrir y leer fichero dependiendo del boton
 
-if ( 0 == filesize($nombreFichero) ){
-    echo "<h1>Hay que añadir preguntas</h1>";
+if (!isset($_POST['boton'])){
+    echo "<h1>Elige una de las opciones para jugar</h1>";
 }
 else{
-	while (!feof($fichero)) {
-        $vector=fgets($fichero,1024);
-		$array_preguntas [] = $vector;
-	}
-	fclose($fichero);
-	
-	//elegir pregunta y mostrarla
-	do{
-		$numero = rand(0, sizeOf($array_preguntas)-1);
-	}
-	while ($array_preguntas[$numero] == "");
-	
-	echo "<h1>Pregunta: ".$array_preguntas[$numero]."</h1>";
-	
-	//eliminar pregunta
-	$fichero_salida = file($nombreFichero); // Read the whole file into an array
-	unset($fichero_salida[$numero]);
-	file_put_contents("$nombreFichero", implode("", $fichero_salida));
+
+    if ($_POST['boton'] == "pregunta"){
+        $nombreFichero = "preguntas.txt";
     }
+    else if($_POST['boton'] == "probable"){
+        $nombreFichero = "probable.txt";
+    }
+
+    $fichero = fopen($nombreFichero, "r");
+    
+    if ( 0 == filesize($nombreFichero) ){
+        echo "<h1>Hay que añadir preguntas</h1>";
+    }
+    else{
+        while (!feof($fichero)) {
+            $vector=fgets($fichero,1024);
+            $array_preguntas [] = $vector;
+        }
+        fclose($fichero);
+        
+        //elegir pregunta y mostrarla
+        do{
+            $numero = rand(0, sizeOf($array_preguntas)-1);
+        }
+        while ($array_preguntas[$numero] == "");
+        
+        echo "<h1>Pregunta: ".$array_preguntas[$numero]."</h1>";
+        
+        //eliminar pregunta
+        $fichero_salida = file($nombreFichero); // Read the whole file into an array
+        unset($fichero_salida[$numero]);
+        file_put_contents("$nombreFichero", implode("", $fichero_salida));
+    }
+    
+}
 
 ?>
